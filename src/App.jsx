37fc9131c7af6aca4187dffcabb5fc6133e3233c
Leafmw <ankang4869@gmail.com>
2024-12-31 14:13:47 +0800

    添加tailwind与etherjs 实现了简单的功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { ethers } from "ethers"
+import { useState } from "react";
+
 function App() {
+
+  const [balance, setBalance] = useState()
+  const provider = ethers.getDefaultProvider()
+
+  const getVmoney = async () => {
+    setBalance("加载中...")
+    const getBalance = await provider.getBalance(`vitalik.eth`);
+    setBalance(ethers.formatEther(getBalance))
+  }
+
   return (
-    <div>初始化项目</div>
+    <div className="w-full h-screen flex">
+      <div className="m-auto w-1/2 h-1/2 bg-gray-400 flex flex-col justify-center items-center">
+        <button onClick={getVmoney} className="w-40 h-10 bg-slate-300">点击获取V神钱包余额</button>
+        <div>v神钱包余额：{balance}</div>
+      </div>
+    </div>
   )
 }
 
